38c33ab1ec36d6728ff3ab5caa788ceaf175abc8
Gaurav Kumar <95544839+NextGenGk@users.noreply.github.com>
2022-07-01 11:28:39 +0530

    Add Find Common in Three Sorted Array

---
+++ Practice/Find_Common_in_Three_Sorted_Array.java
@@ -0,0 +1,38 @@
+package Practice;
+
+public class Find_Common_in_Three_Sorted_Array {
+
+    static void findCommon(int[] arr1, int[] arr2, int[] arr3) {
+        int i=0, j=0, k=0;
+
+        while (i < arr1.length && j < arr2.length && k < arr3.length) {
+            if (arr1[i] == arr2[j] && arr2[j] == arr3[k]) {
+                System.out.print(arr1[i] + " ");
+                i++;
+                j++;
+                k++;
+            }
+
+            else if (arr1[i] < arr2[j]) {
+                i++;
+            }
+
+            else if (arr2[j] < arr3[k]) {
+                j++;
+            }
+            else {
+                k++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Find_Common_in_Three_Sorted_Array ob = new Find_Common_in_Three_Sorted_Array();
+        int[] arr1 = { 1, 5, 10, 20, 40, 80 };
+        int[] arr2 = { 6, 7, 20, 80, 100 };
+        int[] arr3 = { 3, 4, 15, 20, 30, 70, 80, 120 };
+
+        System.out.print("Common elements are ");
+        ob.findCommon(arr1, arr2, arr3);
+    }
+}
